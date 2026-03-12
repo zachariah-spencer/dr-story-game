c12d9696ceb98060632384615a55d83e55b799df
zachariah-spencer <spencerzachariah@gmail.com>
2026-03-11 19:58:06 -0500

    Add spacey particles

--- app/main.rb
+++ app/main.rb
@@ -1,15 +1,37 @@
 def boot args
   args.state = {}
-  
 end
 
+$FONT = "fonts/merriweather.ttf"
+
 def tick args
-  args.outputs.background_color = [20,20,20]
-  args.outputs.solids << text_box
+  args.outputs.background_color = [15,15,15]
+
+  args.state.particles ||= []
+  
+  if args.state.particles.empty?
+    50.times_with_index do |i|
+      scale = Numeric.rand(0.5..3.0)
+      args.state.particles << { 
+        id: "particle_#{i}", 
+        x: Numeric.rand(0..1280),
+        y: Numeric.rand(0..720),
+        dx: Numeric.rand(-0.25..0.25),
+        dy: Numeric.rand(-0.25..0.25),
+        w: scale,
+        h: scale,
+        r: 210,
+        g: 210,
+        b: 210
+      }
+    end
+  end
+
 
   args.state.current_message ||= nil
   args.state.displayed_message ||= ""
   args.state.new_character_tick ||= 0
+  args.state.message_character_offset ||= 0
   Speeds ||= {
     TURTLE: 0.5.seconds,
     SLOW: 0.25.seconds,
@@ -24,23 +46,33 @@ def tick args
     LOUD: 3
   }
 
+  Sizes ||= {
+    SMALL: 3,
+    MEDIUM: 10,
+    LARGE: 20,
+    HUGE: 30
+  }
+
   
   args.state.message_test ||= {
     text: "ONE: This is a test message.",
     speed: Speeds.SLOW,
-    expression: Expressions.NORMAL
+    expression: Expressions.NORMAL,
+    size: Sizes.MEDIUM,
   }
 
   args.state.message_test_two ||= {
     text: "TWO: Just another one of my silly little messages",
     speed: Speeds.MEDIUM,
-    expression: Expressions.NORMAL
+    expression: Expressions.NORMAL,
+    size: Sizes.MEDIUM,
   }
 
   args.state.message_test_three ||= {
-    text: "THREE: A third message that is extremely fast.",
+    text: "12345678901234567890123456789012345678901234567890123456789012345678",
     speed: Speeds.FAST,
-    expression: Expressions.NORMAL
+    expression: Expressions.NORMAL,
+    size: Sizes.MEDIUM,
   }
 
   args.state.messages ||= []
@@ -49,9 +81,25 @@ def tick args
   queue_message args.state.message_test_two, args if args.inputs.keyboard.key_down.b
   queue_message args.state.message_test_three, args if args.inputs.keyboard.key_down.c
 
-  puts args.state.messages
+  calc_particles args
+  args.state.particles.each do |particle|
+    args.outputs.solids << particle
+  end
 
   play_message_queue args
+end
+
+def calc_particles args
+  args.state.particles.each do |particle|
+    particle.x += particle.dx
+    particle.y += particle.dy
+
+    particle.x = 0 if particle.x > 1280
+    particle.x = 1280 if particle.x < 0
+
+    particle.y = 0 if particle.y > 720
+    particle.y = 720 if particle.y < 0
+  end
 end
 
 def text_box
@@ -78,11 +126,15 @@ end
 
 def play_message_queue args
   args.outputs.labels << {
-    x: 200, 
-    y: 500, 
+    x: (Grid.w / 2) - 8.6 * args.state.message_character_offset, 
+    y: Grid.h - 32, 
     text: args.state.displayed_message,
-    size_enum: 10,
-    r: 255
+    alignment_enum: 0,
+    size_enum: 8,
+    font: $FONT,
+    r: 210,
+    g: 210,
+    b: 210
   }
 
   if args.state.current_message
@@ -100,6 +152,7 @@ def play_message_queue args
   elsif args.state.messages.size > 0 && args.inputs.keyboard.key_down.space
     args.state.displayed_message = ""
     args.state.current_message = args.state.messages.pop
+    args.state.message_character_offset = args.state.current_message.text.size if args.state.current_message
     
   end
 end
